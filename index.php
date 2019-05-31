b6abe5dbf6561dcc62a7f78f279654e0bbc130a0
zamzamicode <mzaimzamzami92@gmail.com>
2019-05-31 22:43:39 +0700

    firs commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="header"> 
+            <div class="logo">
+                <img src="image/logo/logo.png" class="" alt="">
+            </div>
+            <div class="title">
+                <h2>Cahkiyeng Test<br>Exam</h2>
+            </div>
+            <div class="header-left">
+                <div class="profil">
+                    <div class="foto">
+                        
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </header>
+</body>
+</html>
